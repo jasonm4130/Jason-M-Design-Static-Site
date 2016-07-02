a4c45001396b04b440f139adb0754e24c94f4cd2
jasonm4130 <jasonm4130@gmail.com>
2016-07-02 12:08:56 +1000

    Better error handeling

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,27 @@ var cp          = require('child_process');
 var pug         = require('gulp-pug');
 var imagemin    = require('gulp-imagemin');
 var cache       = require('gulp-cache');
+var plumber     = require('gulp-plumber');
+var gutil       = require('gulp-util');
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Better Error Handeling
+ */
+var gulp_src = gulp.src;
+gulp.src = function() {
+ return gulp_src.apply(gulp, arguments)
+   .pipe(plumber(function(error) {
+     // Output an error message
+     gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+     // emit the end event, to properly end the task
+     this.emit('end');
+   })
+ );
 };
 
 /**
